87743291211a9663f67ed0cd6a02d7a32068b39a
GPSVIRK <gpsvirk1@gmail.com>
2026-03-13 09:04:06 +0530

    feat: get posts for user

--- scraper.go
+++ scraper.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/GPSVIRK/Rss_Aggregator/internal/database"
+	"github.com/google/uuid"
 )
 
 func startScraping(db *database.Queries,
@@ -46,8 +49,37 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed){
 		log.Println("Error fetching feed:",err)
 		return
 	}
+
 	for _, item := range rssFeed.Channel.Item{
-		log.Println("Found post:",item.Title)
+		description:= sql.NullString{};
+		if item.Description != ""{
+			description.String = item.Description
+			description.Valid = true
+		}
+
+		pubAt, err := time.Parse(time.RFC1123Z, item.PubDate)
+		//this logic could be more robust to allow for all types of time formats
+		if err!= nil{
+			log.Printf("Couldn't parse date %v with err %v", item.PubDate, err)
+			continue
+		}
+
+		_, err = db.CreatePost(context.Background(), database.CreatePostParams{
+			ID: uuid.New(),
+			CreatedAt: time.Now().UTC(),
+			UpdatedAt: time.Now().UTC(),
+			Title: item.Title,
+			Description: description,
+			PublishedAt: pubAt,
+			Url: item.Link,
+			FeedID: feed.ID,
+		})
+		if err != nil{
+			if strings.Contains(err.Error(), "duplicate key"){
+				continue
+			}
+			log.Println("Failed to create post:",err)
+		}
 	}
 	log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
 }
